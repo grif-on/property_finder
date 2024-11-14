6785c1eac725997867c7685be1a64d752cf61607
Grif_on <91139654+grif-on@users.noreply.github.com>
2024-11-14 22:10:36 +0300

    Changed default shortcuts

--- property_finder.js
+++ property_finder.js
@@ -162,6 +162,7 @@ const find_property = tiled.registerAction(shared_name_find_property, function (
 
 find_property.text = shared_name_find_property;
 find_property.icon = "find_p.png";
+find_property.shortcut = "Ctrl+Alt+F";
 
 tiled.extendMenu("Map", [
     { action: shared_name_find_property, before: "SelectNextTileset" }
@@ -212,7 +213,7 @@ const find_value_in_property = tiled.registerAction(shared_name_find_value_in_pr
 
 find_value_in_property.text = shared_name_find_value_in_property;
 find_value_in_property.icon = "find_v_in_p.png";
-find_value_in_property.shortcut = "Ctrl+Alt+F";
+find_value_in_property.shortcut = "Ctrl+Shift+F";
 
 tiled.extendMenu("Map", [
     { action: shared_name_find_value_in_property, before: "SelectNextTileset" }
